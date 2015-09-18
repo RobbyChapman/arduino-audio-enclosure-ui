8381ad0a920209a7836402d05c7ee15f58e280aa
Robby Chapman <robert.chapman@kronos.com>
2015-09-18 08:24:14 -0400

    Adding UTFT lib. Add initial sketch.

---
+++ src/EnclosureUI/EnclosureUI.ino
@@ -0,0 +1,11 @@
+/* UI for audio project displaying voltage, amperage, temperature, and humidity of enclosed enviorment(3/4" plywood) */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
