d5fac495ee48fa8816da7e4c213507a23b1e3fdf
Robby Chapman <robert.chapman@kronos.com>
2015-09-21 18:09:26 -0400

    Adding grid && column layout with centered headers.

--- src/EnclosureUI/EnclosureUI.ino
+++ src/EnclosureUI/EnclosureUI.ino
@@ -1,11 +1,103 @@
 /* UI for audio project displaying voltage, amperage, temperature, and humidity of enclosed enviorment(3/4" plywood) */
 
+#include <memorysaver.h>
+#include <UTFT.h>
+
+/* Designitated initializer for UTFT: (chipset,RS,WR,CS,RST) */
+UTFT lcd(TFT01_43,38,39,40,41);
+
+extern uint8_t SmallFont[];
+extern uint8_t BigFont[];
+
 void setup() {
-  // put your setup code here, to run once:
-
+  
+  lcd.clrScr();
+  lcd.InitLCD(LANDSCAPE);
+  lcd.setFont(BigFont);
+  lcd.setContrast(30);
+  lcd.fillScr(VGA_BLACK);
+  lcd.setColor(VGA_GREEN);
+  drawSensorGrid();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int getFontWidth() {
 
+    return lcd.getFontXsize();
 }
+
+int getFontHeight() {
+
+    return lcd.getFontYsize();
+}
+
+int getMaxWidth() {
+
+  return lcd.getDisplayXSize(); //480
+}
+
+int getMaxHeight() {
+
+  return lcd.getDisplayYSize(); //272
+}
+
+int getHeaderHeight() {
+
+    return 45;
+}
+
+int getBorderWidth() {
+
+  return 10;
+}
+
+int getHeaderYInset() {
+
+  return 60;
+}
+
+void drawSensorGrid() {
+  
+  drawContainer();
+  drawColumnsWithContext();
+}
+
+void drawColumnsWithContext() {
+
+ char* columnTitles[] = {"V", "A", "T", "H"}; // Pass this in!
+ int columnTitlesLength = sizeof(columnTitles) / sizeof(char *);
+ int columnWidth = getMaxWidth()/columnTitlesLength;
+ int yPos = getHeaderYInset() + getBorderWidth();
+ int titleYInset = 20;
+
+ /* Draw Columns */
+  for (int i = 0; i < columnTitlesLength; i++) {
+    lcd.fillRect(columnWidth * i, yPos, columnWidth * i + getBorderWidth(), getMaxHeight());
+    /* Calculate column width for context X coordinate */
+    int xPos = ((columnWidth * i) + columnWidth/2) - (getFontWidth()/2);
+    lcd.print(columnTitles[i], xPos, yPos + titleYInset);
+  }
+}
+
+void drawContainer() {
+
+  /* Set container style */
+  lcd.setColor(VGA_RED);
+  lcd.setFont(SmallFont); // <-- Pick a bigger font!
+
+  /* Draw header */
+  lcd.fillRect(0, getHeaderYInset(), getMaxWidth() - 1, getHeaderYInset() + getBorderWidth());
+
+  /* Draw top edge */
+  lcd.fillRect(0, 0 , getMaxWidth() -1, getBorderWidth());
+  
+  /* Draw bottom edge */
+  lcd.fillRect(0, getMaxHeight() - getBorderWidth(), getMaxWidth() - 1, getMaxHeight());
+
+  /* Draw left edge */
+  lcd.fillRect(0, 0, getBorderWidth(), getMaxHeight());
+
+  /* Draw right edge */
+  lcd.fillRect(getMaxWidth() - 1, 0, getMaxWidth() - getBorderWidth() , getMaxHeight());
+}
+
+void loop() {}
